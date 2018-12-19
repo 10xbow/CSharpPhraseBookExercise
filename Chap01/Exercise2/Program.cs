8f37c42e8dbaf5d3718fe60631be093ce9b7ea77
當房一樹 <toubou@techno-d.jp>
2018-12-19 17:48:05 +0900

    1.2終了

--- Chap01/Exercise2/Program.cs
+++ Chap01/Exercise2/Program.cs
@@ -9,8 +9,12 @@ namespace Exercise2
             var myClass = new MyClass { X = 1, Y = 2 };
             var myStruct = new MyStruct { X = 3, Y = 4 };
 
+            // 1.2.3
+            PrintObjectsTwice(myClass, myStruct);
+
             // 1.2.2
             PrintObjects(myClass, myStruct);
+
             Console.ReadLine();
         }
 
@@ -26,6 +30,24 @@ namespace Exercise2
                 var properties = obj.GetType().GetProperties();
                 foreach (var property in properties)
                 {
+                    Console.WriteLine($"{property.Name}: {property.GetValue(obj)}");
+                }
+            }
+        }
+
+        // 1.2.3
+        static void PrintObjectsTwice(MyClass myClass, MyStruct myStruct)
+        {
+            ShowObjectInfo(myClass);
+            ShowObjectInfo(myStruct);
+
+            void ShowObjectInfo(object obj)
+            {
+                Console.WriteLine(obj.GetType());
+                var properties = obj.GetType().GetProperties();
+                foreach (var property in properties)
+                {
+                    property.SetValue(obj, (int)property.GetValue(obj) * 2);
                     Console.WriteLine($"{property.Name}: {property.GetValue(obj)}");
                 }
             }
