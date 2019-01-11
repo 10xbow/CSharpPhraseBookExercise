9eba9a34d950640a48f19ef951fa5ed8c1bae315
當房一樹 <toubou@techno-d.jp>
2019-01-11 10:24:02 +0900

    Chap11完了

--- Chap11/Exercise1/Program.cs
+++ Chap11/Exercise1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace Exercise1
@@ -12,7 +9,37 @@ namespace Exercise1
         static void Main(string[] args)
         {
             var xdoc = XDocument.Load("Sample.xml");
+            var xelements = xdoc.Root.Elements();
 
+            // 11.1.1
+            xelements.Select(x => new
+                        {
+                            Name = (string)x.Element("name"),
+                            Teammembers = (int)x.Element("teammembers")
+                        })
+                     .ToList()
+                     .ForEach(x => Console.WriteLine($"{x.Name}\t{x.Teammembers.ToString()}"));
+
+            // 11.1.2
+            xelements.OrderBy(x => (int)x.Element("firstplayed"))
+                     .ToList()
+                     .ForEach(x => Console.WriteLine(x.Element("name").Attribute("kanji").Value));
+
+            // 11.1.3
+            xelements.OrderByDescending(x => (int)x.Element("teammembers"))
+                     .Take(1)
+                     .ToList()
+                     .ForEach(x => Console.WriteLine($"{(string)x.Element("name")}"));
+
+            // 11.1.4
+            var element = new XElement("ballsport",
+                new XElement("name", "サッカー", new XAttribute("kanji", "蹴球")),
+                new XElement("teammembers", "11"),
+                new XElement("firstplayed", "1863")
+                );
+
+            xdoc.Root.Add(element);
+            xdoc.Save("addSoccer.xml");
         }
     }
 }
---
+++ Chap11/Exercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xdoc = XDocument.Load("sample.xml");
+            var dict = xdoc.Root.Elements().ToDictionary(x => (string)x.Element("kanji"), x => (string)x.Element("yomi"));
+            xdoc.Root.RemoveAll();
+            foreach (var item in dict)
+            {
+                var element = new XElement("word", new XAttribute("kanji", item.Key), new XAttribute("yomi", item.Value));
+                xdoc.Root.Add(element);
+            }
+            xdoc.Save("result.xml");
+        }
+    }
+}
